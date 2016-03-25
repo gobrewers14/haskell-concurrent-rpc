af17ee6e25776398e8ff02c5d21db49a2c3d5068
Lars Petersen <info@lars-petersen.net>
2016-03-25 12:20:48 +0100

    Improved documentation.

--- src/Control/Concurrent/RPC.hs
+++ src/Control/Concurrent/RPC.hs
@@ -6,54 +6,81 @@
 --
 -- Maintainer  :  info@lars-petersen.net
 -- Stability   :  experimental
---
--- /Example1:/ Use `RPC` to synchronise resource access (like printing to the
--- terminal) of concurrent threads. Compare the `RPC` solution
---
--- > import Control.Monad (forever)
--- > import Control.Concurrent
--- > import Control.Concurrent.RPC
--- >
--- > main :: IO ()
--- > main = do
--- >   (log, withLogMessage) <- newRPC
--- >   forkIO $ forever $ log "<<<<<<<"
--- >   forkIO $ forever $ log ">>>>>>>"
--- >   forever $ withLogMessage print
--- >
--- > -- Example output:
--- > --
--- > -- "<<<<<<<"
--- > -- ">>>>>>>"
--- > -- "<<<<<<<"
--- > -- ">>>>>>>"
---
--- with this counter example which has a little issue with concurrency:
---
--- > import Control.Monad (forever)
--- > import Control.Concurrent
--- >
--- > main :: IO ()
--- > main = do
--- >   forkIO $ forever $ print "<<<<<<<"
--- >   forever $ print ">>>>>>>"
--- >
--- > -- Example output:
--- > --
--- > -- ""><><><><><><><""
--- > --
--- > -- ""><><><><><><><""
--- > --
--- > -- ""><><><><><><><""
 -----------------------------------------------------------------------------
 module Control.Concurrent.RPC where
 
 import Control.Exception
 import Control.Concurrent.MVar
 
+-- | The client interface for threads invoking /remote procedure calls/.
+--
+--   * The operation blocks until another thread processed the request.
+--   * If the other thread throws an exception during processing, the exception
+--     is re-thrown in the thread waiting for the response.
+--
+-- > type Rocket  = String
+-- > type Liftoff = Bool
+-- >
+-- > main = do
+-- >   (launchRocket, withRocket) <- newRPC :: IO (RPC Rocket Liftoff, WithRPC Rocket Liftoff)
+-- >   ..
+-- >   catch
+-- >     ( launchRocket "Apollo 11" >>= \liftoff-> if liftoff
+-- >         then print "Houston, we have a liftoff!"
+-- >         else print "Launch cancelled!"
+-- >     )
+-- >     ( \e-> print $ "Houston, we have a problem: " ++ show (e :: IOError) )
 type RPC     request response = request -> IO response
+
+-- | The interface for threads that serve and process /remote procedure calls/.
+--
+--   * More than one thread may be used to process requests all using the same
+--     interface object. All processing threads will block on `Control.Concurrent.take`
+--     on the same `MVar` and only one will be served at a time. Fairness
+--     properties of `Control.Concurrent.MVar` apply.
+--   * Exceptions thrown by the handler operation will be re-thrown in both
+--     the processing thread and the requesting thread.
+--
+-- > type Rocket  = String
+-- > type Liftoff = Bool
+-- >
+-- > main = do
+-- >   (launchRocket, withRocket) <- newRPC :: IO (RPC Rocket Liftoff, WithRPC Rocket Liftoff)
+-- >   ..
+-- >   -- This is the rocket launch site thread. It forever waits for rockets and fires them into space one after the other.
+-- >   forkIO $ withFile "/dev/null" WriteMode $ \space->
+-- >     forever $ catch
+-- >       ( withRocket $ \rocket-> do
+-- >           weather <- getWeatherCondition
+-- >           when (isGood weather) $
+-- >             hPutStrLn space rocket -- The actual launch may throw an IOError!
+-- >           return weather
+-- >       )
+-- >       ( \e-> print "A rocket exploded during launch phase: " ++ (e :: IOError) )
 type WithRPC request response = (request -> IO response) -> IO ()
 
+-- | Creates a new request-response communication channel that may be used
+--   by arbitrary many requesting and/or processing threads.
+--
+-- > main :: IO ()
+-- > main = do
+-- >  (rpc, withRpc) <- newRPC
+-- >  forkIO $ forever $ withRpc $ \request->
+-- >    response <- doSomethingWith request
+-- >    return response
+-- >  response <- rpc request
+-- >  ..
+--
+--   * `newRPC` initially creates one empty `Control.Concurrent.MVar.MVar`
+--     for queueing requests.
+--   * Each call of `rpc` creates a temporary `Control.Concurrent.MVar` for the
+--     reponse.
+--   * If the handler given to `withRpc` throws an exception, the exception is
+--     re-thrown in the `withRpc` thread as well as in the `rpc` thread that
+--     issued the call.
+--   * If an `rpc` thread that issued a call dies before the request
+--     processing has started, the request gets discarded. If processing has
+--     already started, the processing will finish and the response gets discarded.
 newRPC :: IO (RPC request response, WithRPC request response)
 newRPC = do
   requestMVar <- newEmptyMVar
